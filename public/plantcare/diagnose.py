e85b68dcaeac50c5f03753596a51749af901edd0
Kevin Damstra <kevin@lumideers.nl>
2014-12-04 12:18:01 +0100

    hey

--- public/plantcare/diagnose.py
+++ public/plantcare/diagnose.py
@@ -1,21 +1,13 @@
-# import io module from scikit-image for opening/reading images from filesystem
 from skimage import io
 import sys
-# import our awesome homemade PlantDoctor class
 from includes.plantdoctor import PlantDoctor
-# import the flask web framework modules
 
 def post_response(image_path):
-        # read the image as NumPy array
-        print image_path;
         img = io.imread(image_path)
-        # create new instance of our awesome PlantDoctor
         doctor = PlantDoctor()
-        # diagnose uploaded image
         diagnosis = doctor.diagnose(img)
         print "%s" %diagnosis
 
-# pythonic equivalent of init() function for this script
 if __name__ == '__main__':
     image_path = sys.argv[1];
     post_response(image_path)
--- public/plantcare/includes/plantdoctor.py
+++ public/plantcare/includes/plantdoctor.py
@@ -4,14 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 # import MySQL module for database access
 import MySQLdb as mdb
+import os
+import copy
 
 # import needed scikit-image modules
-from skimage import io, color
-import sys, os
+from skimage import io 
 from skimage.color import rgb2hsv, rgb2gray
 from skimage.measure import structural_similarity as ssim
 from skimage.transform import resize
 from skimage.filter import gaussian_filter as gf
+from scipy import ndimage
+from skimage.filter import threshold_otsu as otsu
+from skimage.morphology import disk, erosion 
 
 script_root = os.path.dirname(os.path.realpath(__file__));
 
@@ -25,23 +29,65 @@ class PlantDoctor:
 
         # create a local dataset to use for diagnosis
         self.dataset = []
+        self.images = []
         for url, disease in db_images:
             img_disease = io.imread(script_root+'/'+url)
             self.dataset.append([img_disease, disease])
 
+    def extract_leaf(self, img):
+        img_gray = rgb2gray(img)
+
+        otsu_mean = otsu(img_gray)
+        otsu_img = img_gray < otsu_mean
+        io.imsave("Otsu.png", otsu_img)
+        self.images.append([otsu_img, "Otsu"])
+
+        eroded_img = ndimage.binary_fill_holes(erosion(otsu_img, disk(1)))
+
+        markers = ndimage.label(eroded_img)[0]
+
+        self.images.append([markers, "Markers"])
+        io.imsave("Markers.png", otsu_img)
+        
+        markers_copy = markers.astype(float)
+        markers_copy[markers_copy == 0] = np.nan
+        axis = 1
+        u, indices = np.unique(markers_copy, return_inverse=True)
+        u = u[np.nanargmax(np.apply_along_axis(np.bincount, axis, indices.reshape(markers_copy.shape),
+                                            None, np.max(indices) + 1), axis=axis)]
+        u, indices = np.unique(u, return_inverse=True)
+
+        leaf_marker = int(u[np.argmax(np.bincount(indices))])
+        img_mask = img
+
+        for row, column in enumerate(markers[:,:]):
+            for index, value in enumerate(column):
+                if value != leaf_marker:
+                    for subindex, value in enumerate(img[row,index,:]):
+                        img_mask[row,index,subindex] = 255
+
+        return img_mask
+
     def diagnose(self, img):
-        # resize image to speed up analysis
+        self.images.append([img, "Original"])
+        io.imsave("Original.png", img)
+        #TODO resize image to speed up analysis (determine breaking point for quality)
+        img_extracted = self.extract_leaf(img)
+        self.images.append([img_extracted, "Extracted Leaf"])
+        io.imsave("Extracted.png", img_extracted)
         # set diseased pixels to 1, and healthy pixels to 0 (binary representation of regions)
-        img_bin = np.where(rgb2hsv(img) < 0.125, 1.0, 0.0)
+        img_bin = np.where(rgb2hsv(img_extracted) < 0.15, 1.0, 0.0)
 
         # create a masked image from the binary image above
-        img_mask = img
+        img_mask = copy.deepcopy(img)
         for row, column in enumerate(img_bin[:,:,0]):
             for index, value in enumerate(column):
-                if value == 0:
+                if value == 0 and np.mean(img_extracted[row, index]) != 255:
                     for subindex, value in enumerate(img[row,index,:]):
-                        img_mask[row,index,subindex] = 0
+                        img_mask[row,index,subindex] = 255
 
+        self.images.append([img_mask, "Extracted Disease"])
+        io.imsave("Extracted_disease.png", img_mask)
         """
             create a SSIM dictionary by comparing the uploaded image with our local dataset.
             SSIM documentation: http://en.wikipedia.org/wiki/Structural_similarity
@@ -69,6 +115,7 @@ class PlantDoctor:
                     highest_similarity = [disease, match]
 
         # return the name of the diagnosed disease
+        self.show_images()
         return str(highest_similarity[0])
 
     def get_images(self):
@@ -92,12 +139,12 @@ class PlantDoctor:
 
             print "Error %d: %s" % (e.args[0],e.args[1])
 
-    def show_images(self,images,titles=None):
-        numb_of_imgs = len(images)
-        if titles is None: titles = ['(%d)' % i for i in range(1,numb_of_imgs + 1)]
+    def show_images(self):
+        numb_of_imgs = len(self.images)
+        print numb_of_imgs
         fig = plt.figure()
         n = 1
-        for image,title in zip(images,titles):
+        for image,title in self.images:
             a = fig.add_subplot(1,numb_of_imgs,n)
             if image.ndim == 2:
                 plt.gray()
